e37f264bf8640ef163e25158c11a2fb28a06f237
Oscar Arzola <oscararzola@gmail.com>
2024-01-24 12:26:52 -0500

    fix: build process

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,5 @@
 import create_config from '@kucrut/vite-for-wp';
 
 export default create_config( {
-	app: 'resources/assets/js/app.js',
+	app: 'resources/assets/js/pressbooks-plugin-scaffold.js',
 }, 'dist' );
