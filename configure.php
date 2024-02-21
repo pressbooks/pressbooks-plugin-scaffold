04918f6a7f3514829f301642b1a1e62905b5eeff
Felipe Dalcin <felipe@pressbooks.com>
2024-02-21 14:05:04 -0500

    feat: configure script

---
+++ configure.php
@@ -0,0 +1,116 @@
+#!/usr/bin/env php
+<?php
+
+function ask(string $question, string $default = ''): string
+{
+    $answer = readline($question . ($default ? " ({$default})" : null) . ': ');
+
+    if (! $answer) {
+        return $default;
+    }
+
+    return $answer;
+}
+
+function confirm(string $question, bool $default = false): bool
+{
+    $answer = ask($question .' (' . ($default ? 'Y/n' : 'y/N') . ')');
+
+    if (! $answer) {
+        return $default;
+    }
+
+    return strtolower($answer) === 'y';
+}
+
+function writeln(string $line): void
+{
+    echo $line.PHP_EOL;
+}
+
+function slugify(string $subject): string
+{
+    return strtolower(trim(preg_replace('/[^A-Za-z0-9-]+/', '-', $subject), '-'));
+}
+
+function titleCase(string $subject): string
+{
+    return str_replace(' ', '', ucwords(str_replace(['-', '_'], ' ', $subject)));
+}
+
+function replaceInFile(string $file, array $replacements): void
+{
+    $contents = file_get_contents($file);
+
+    file_put_contents($file, str_replace(
+        array_keys($replacements),
+        array_values($replacements),
+        $contents
+    ));
+}
+
+function separator(string $path): string
+{
+    return str_replace('/', DIRECTORY_SEPARATOR, $path);
+}
+
+function run(string $command): string
+{
+    return trim((string) shell_exec($command));
+}
+
+function getReplaceableFiles(): array
+{
+    $namedFiles = explode(PHP_EOL, run('find ./* -name "pressbooks-plugin-scaffold*"'));
+
+    $contentFiles = explode(PHP_EOL, run('grep -E -r -l -i "PressbooksPluginScaffold|pressbooks-plugin-scaffold" --exclude-dir=vendor ./* ./.github/* | grep -v '.basename(__FILE__)));
+
+    return array_unique(
+        array_merge($namedFiles, $contentFiles)
+    );
+}
+
+// --------------- SCRIPT START --------------- //
+$currentDirectory = getcwd();
+
+$pluginName = ask('Plugin name', basename($currentDirectory));
+
+$snakeCaseName = slugify($pluginName);
+$titleCaseName = ask('Namespace name', titleCase($pluginName));
+$description = ask('Plugin description', "This is my plugin {$snakeCaseName}");
+
+writeln('------');
+writeln("Plugin         : {$snakeCaseName} <{$description}>");
+writeln("Namespace      : Pressbooks\\{$titleCaseName}");
+writeln("Class name     : {$titleCaseName}");
+writeln("Root file name : {$snakeCaseName}");
+writeln('------');
+
+writeln('This script will replace the above values in all relevant files in the project directory.');
+
+if (! confirm('Modify files?', true)) {
+    exit(1);
+}
+
+$files = getReplaceableFiles();
+
+foreach ($files as $file) {
+    replaceInFile($file, [
+        'pressbooks-plugin-scaffold' => $snakeCaseName,
+        'PressbooksPluginScaffold' => $titleCaseName,
+        'A scaffold for Pressbooks plugins.' => $description,
+    ]);
+
+    match (true) {
+        str_contains($file, separator('resources/assets/js/pressbooks-plugin-scaffold.js')) => rename($file, separator('./resources/assets/js/' . $snakeCaseName . '.js')),
+        str_contains($file, separator('resources/assets/css/pressbooks-plugin-scaffold.css')) => rename($file, separator('./resources/assets/css/' . $snakeCaseName . '.css')),
+        str_contains($file, separator('pressbooks-plugin-scaffold.php')) => rename($file, separator('./' . $snakeCaseName . '.php')),
+        default => null,
+    };
+}
+
+confirm('Execute `composer install`?') && run('composer install');
+
+confirm('Let this script delete itself?', true) && unlink(__FILE__);
+
+writeln('Script completed.');
